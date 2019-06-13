8cf7a3862bc3ffbe7acb16a1ec36b761499125d7
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-13 23:37:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  counter = 0
+  result =[]
+
+  while counter < array.length do
+        result << array[counter] ** 2
+        counter += 1
+  end
+  result
 end
